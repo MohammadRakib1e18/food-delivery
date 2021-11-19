587365db475c55d51ecbb6f15e43e0482bb4f711
MohammadRakib1e18 <trustedrakib@gmail.com>
2021-11-19 20:18:29 +0600

    added

--- src/Components/MyOrders/MyOrders.js
+++ src/Components/MyOrders/MyOrders.js
@@ -7,6 +7,7 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { Button } from "@mui/material";
 
 const MyOrders = () => {
     const [orders, setOrders] = useState([]);
@@ -18,19 +19,59 @@ const MyOrders = () => {
             .then((res) => res.json())
             .then((data) => setOrders(data));
     }, []);
+    // DELETE AN USER
+    const handleDeleteUser = (id) => {
+        const agreeToDelete = window.confirm(
+            "Are you sure, you want to delete?"
+        );
+        console.log(agreeToDelete);
+        if (agreeToDelete) {
+            const url = `https://morning-harbor-17755.herokuapp.com/deleteOrder/${id}`;
+            fetch(url, {
+                method: "DELETE",
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log("data: ", data);
+                    if (data.deletedCount > 0) {
+                        alert("deleted successfully");
+                        const remainingOrders = orders.filter(
+                            (order) => order._id !== id
+                        );
+                        setOrders(remainingOrders);
+                    }
+                });
+        }
+    };
     return (
         <div>
-            <h2 className="my-4"><span className="text-danger">{user.displayName}'s</span> <span className="text-success">order</span></h2>
-            <hr/>
+            <h2 className="my-4">
+                <span className="text-danger">{user.displayName}'s</span>{" "}
+                <span className="text-success">order</span>
+            </h2>
+            <hr />
             <TableContainer component={Paper}>
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
                         <TableRow>
-                            <TableCell sx={{fontSize: '25px'}}>Food</TableCell>
-                            <TableCell align="right" sx={{fontSize: '25px'}}>Food Name</TableCell>
-                            <TableCell align="right" sx={{fontSize: '25px'}}>Phone</TableCell>
-                            <TableCell align="right" sx={{fontSize: '25px'}}>E-mail</TableCell>
-                            <TableCell align="right" sx={{fontSize: '25px'}}>Price</TableCell>
+                            <TableCell sx={{ fontSize: "25px" }}>
+                                Food
+                            </TableCell>
+                            <TableCell align="right" sx={{ fontSize: "25px" }}>
+                                Food Name
+                            </TableCell>
+                            <TableCell align="right" sx={{ fontSize: "25px" }}>
+                                Phone
+                            </TableCell>
+                            <TableCell align="right" sx={{ fontSize: "25px" }}>
+                                E-mail
+                            </TableCell>
+                            <TableCell align="right" sx={{ fontSize: "25px" }}>
+                                Price
+                            </TableCell>
+                            <TableCell align="right" sx={{ fontSize: "25px" }}>
+                                Action
+                            </TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
@@ -44,19 +85,54 @@ const MyOrders = () => {
                                 }}
                             >
                                 <TableCell component="th" scope="row">
-                                    <img src={order.picture} style={{width:'300px'}} alt={order.name}/>
+                                    <img
+                                        src={order.picture}
+                                        style={{
+                                            width: "300px",
+                                            padding: "0px",
+                                        }}
+                                        alt={order.name}
+                                    />
                                 </TableCell>
-                                <TableCell style={{fontSize: '1em'}} align="right">
+                                <TableCell
+                                    style={{ fontSize: "1em" }}
+                                    align="right"
+                                >
                                     {order.foodName}
                                 </TableCell>
-                                <TableCell style={{fontSize: '1em'}} align="right">
+                                <TableCell
+                                    style={{ fontSize: "1em" }}
+                                    align="right"
+                                >
                                     {order.phone}
                                 </TableCell>
-                                <TableCell style={{fontSize: '1em'}} align="right">
+                                <TableCell
+                                    style={{ fontSize: "1em" }}
+                                    align="right"
+                                >
                                     {order.email}
                                 </TableCell>
-                                <TableCell style={{fontSize: '1em'}} align="right">
+                                <TableCell
+                                    style={{ fontSize: "1em" }}
+                                    align="right"
+                                >
                                     ${order.price}
+                                </TableCell>
+                                <TableCell
+                                    style={{ fontSize: "1em" }}
+                                    align="right"
+                                >
+                                    <Button
+                                        sx={{ width: "50%", m: 1 }}
+                                        type="submit"
+                                        variant="contained"
+                                        color="error"
+                                        onClick={() =>
+                                            handleDeleteUser(order._id)
+                                        }
+                                    >
+                                        Delete
+                                    </Button>
                                 </TableCell>
                             </TableRow>
                         ))}
